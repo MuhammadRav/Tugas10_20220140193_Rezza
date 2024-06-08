32cf1048376441cdd9772e4144330189a6756d47
Muhammad Rezza <126452363+MuhammadRav@users.noreply.github.com>
2024-06-08 17:44:13 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './p.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Asslamualaikum Braderrr</h1>
+      <p>Semoga Masuk Surga</p>
+      <p>Myname - Muhammad Rezza Tri Ariyavani...</p>
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
